aedbd735272cbdf473aa96a65e43ff804d33957b
aiperi <kydykbekovaajperi@gmail.com>
2026-02-17 16:33:36 +0600

    Add fields and methods to Service class

--- src/Service.java
+++ src/Service.java
@@ -4,27 +4,49 @@ public abstract class Service {
     // serviceName
     // serviceId
     // isActive
+    private String name;
+    private int id;
+    private boolean isActive;
 
 
     // TODO: constructor
+    public Service(String name, int id) {
+        this.name = name;
+        this.id = id;
+        this.isActive = false;
+    }
 
 
     // TODO: activateService()
     // should set isActive = true
     // print activation message
+    public void activateService() {
+        isActive = true;
+        System.out.println(getServiceName()+"is now active");
+    }
 
 
     // TODO: deactivateService()
     // should set isActive = false
     // print deactivation message
+    public void deactivateService() {
+        isActive = false;
+        System.out.println(getServiceName()+ "is now not active");
+    }
 
 
     // TODO: getter for serviceName
-
+    public String getServiceName() {return name;}
 
     // TODO: getter for isActive
+    public boolean getisActive() {
+        return isActive;
+    }
+
 
 
     // TODO: abstract method performService()
+
+    public abstract void performService();
 
 }
