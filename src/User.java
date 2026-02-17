aab1584723ef63b02d7c9816254a0648b158dd9e
aiperi <kydykbekovaajperi@gmail.com>
2026-02-17 16:35:20 +0600

    Add fields and constructor to User class

--- src/User.java
+++ src/User.java
@@ -2,15 +2,26 @@ public abstract class User {
 
     // TODO: declare fields
     // name
-    // id
+    //id
+    private String name;
+    private int id;
 
 
     // TODO: constructor
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {return name;}
+
+    public int getId() {return id;}
 
 
     // TODO: abstract method manageService(Service s)
+    public abstract void manageService(Service s);
 
 
     // TODO: abstract method useService(Service s)
-
+    public abstract void useService(Service s);
 }
