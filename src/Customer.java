6ce0e32dabf20f300b1fc2229f4befb058187cbb
aiperi <kydykbekovaajperi@gmail.com>
2026-02-17 16:32:31 +0600

    Add constructor and service management to Customer
    Implemented constructor for Customer class and updated service management methods.

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,16 @@
 public class Customer extends User {
 
     // TODO: constructor
-
+    public Customer(String name, int id){
+        super(name, id);
+    }
 
     @Override
     public void manageService(Service s) {
 
         // TODO:
         // print that customer cannot manage services
+        System.out.println("Customer" +getName()+ "cannot manage services");
 
     }
 
@@ -17,6 +20,8 @@ public class Customer extends User {
         // TODO:
         // print customer using message
         // call performService()
+        System.out.println("Customer" +getName() +"using now"+s.getServiceName());
+        s.performService();
 
     }
 }
